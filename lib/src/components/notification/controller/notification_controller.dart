58f95ae11aafa988739a2a95734067addc0f413b
Seun Odeyemi <seunodeyemi@Clevers-MacBook-Pro.local>
2023-03-07 01:17:36 +0100

    completed notifications

--- lib/src/components/notification/controller/notification_controller.dart
+++ lib/src/components/notification/controller/notification_controller.dart
@@ -1,10 +1,19 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sprout_mobile/src/repository/preference_repository.dart';
+import 'package:sprout_mobile/src/utils/constants.dart';
 import '../service/notification_service.dart';
 
 class NotificationController extends GetxController {
   TextEditingController emailController = new TextEditingController();
+  final PreferenceRepository preferenceRepository =
+      Get.put(PreferenceRepository());
   final push = PushNotificationService();
+
+  String notifications = "";
+  int notification_length = 0;
 
   @override
   void onInit() {
@@ -20,5 +29,10 @@ class NotificationController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+  }
+
+  Future<void> getNotifications() async {
+    notifications = await preferenceRepository.getStringPref(NOTIFICATIONS);
+    notification_length = jsonDecode(notifications)?.length;
   }
 }
